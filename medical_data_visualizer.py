5c0a2013aa5eebf1f2d1d12efb895e2c4540a43c
wittawatTon <wittawat.naranong@gmail.com>
2024-08-14 14:03:51 +0700

    test pass

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,30 +4,30 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv('medical_examination.csv')
 
 # 2
-df['overweight'] = None
+df['overweight'] =  (df['weight'] / ((df['height'] / 100) ** 2)).apply(lambda x: 1 if x > 25 else 0)
 
 # 3
-
+df['cholesterol'] = df['cholesterol'].apply(lambda x: 1 if x > 1 else 0)
+df['gluc'] = df['gluc'].apply(lambda x: 1 if x > 1 else 0)
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
-
+    df_cat = pd.melt(df, id_vars=['cardio'], value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
 
     # 6
-    df_cat = None
-    
+    df_cat = df_cat.groupby(['cardio', 'variable', 'value'], as_index=False).size()
+    df_cat.rename(columns={'size': 'total'}, inplace=True)
 
     # 7
-
-
+    fig = sns.catplot(x='variable', y='total', hue='value', col='cardio', kind='bar', data=df_cat)
 
     # 8
-    fig = None
+    fig.savefig('catplot.png')
+
 
 
     # 9
@@ -37,24 +37,25 @@ def draw_cat_plot():
 
 # 10
 def draw_heat_map():
-    # 11
-    df_heat = None
+  # 11. Clean the data
+    df_heat = df[(df['ap_lo'] <= df['ap_hi']) & 
+                 (df['height'] >= df['height'].quantile(0.025)) &
+                 (df['height'] <= df['height'].quantile(0.975)) &
+                 (df['weight'] >= df['weight'].quantile(0.025)) &
+                 (df['weight'] <= df['weight'].quantile(0.975))]
 
-    # 12
-    corr = None
+    # 12. Calculate the correlation matrix
+    corr = df_heat.corr()
 
-    # 13
-    mask = None
+    # 13. Generate a mask for the upper triangle
+    mask = np.triu(np.ones_like(corr, dtype=bool))
 
+    # 14. Set up the matplotlib figure
+    fig, ax = plt.subplots(figsize=(12, 8))
 
+    # 15. Draw the heatmap
+    sns.heatmap(corr, mask=mask, annot=True, fmt='.1f', cmap='coolwarm', square=True, linewidths=0.5, ax=ax)
 
-    # 14
-    fig, ax = None
-
-    # 15
-
-
-
-    # 16
+    # 16. Save and return the figure
     fig.savefig('heatmap.png')
     return fig
